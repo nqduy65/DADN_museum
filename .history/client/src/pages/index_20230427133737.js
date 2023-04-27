32ba10a5ed764bd44fd2141fc8def5d01ce77688
tranvantai2905 <tai.trantai58@hcmut.edu.vn>
2023-04-27 14:14:44 +0700

    init

---
+++ .history/client/src/pages/index_20230427133737.js
@@ -0,0 +1,7 @@
+export { default as MainPage } from "./Main";
+export { default as SignUpPage } from "./SignUp";
+export { default as LoginPage } from "./Login";
+export { default as Manage } from "./Manage";
+export { default as DashBroad } from "./DashBroad";
+export { default as ExhibitManage } from "./ExhibitManage";
+export { default as RecordSave } from "./RecordSave";
