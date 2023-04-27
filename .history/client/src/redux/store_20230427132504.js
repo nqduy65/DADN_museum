3b84716a45e46a1af9d0b00b96664462cbc23e6a
tranvantai2905 <tai.trantai58@hcmut.edu.vn>
2023-04-27 13:27:33 +0700

    haha

--- .history/client/src/redux/store_20230427132504.js
+++ .history/client/src/redux/store_20230427132504.js
@@ -2,12 +2,8 @@ import { configureStore } from "@reduxjs/toolkit";
 import mainSlice from "../pages/Main/mainSlice";
 import dmanageSlice from "../pages/Manage/dmanage";
 import dashBoardSlice from "../pages/DashBroad/dashBroadSlice";
-<<<<<<< HEAD
-import exhibitSlice  from "../pages/ExhibitManage/exhibitManageSlice";
-import recordSlice from "../pages/RecordSave/recordSlice";
-=======
+
 import exhibitSlice from "../pages/exhibitManage/exhibitManageSlice";
->>>>>>> 100510c2a9295ff209e9226fccb845854fc351e5
 
 const store = configureStore({
   reducer: {
@@ -15,7 +11,7 @@ const store = configureStore({
     dmanage: dmanageSlice.reducer,
     dashboard: dashBoardSlice.reducer,
     exhibit: exhibitSlice.reducer,
-    record: recordSlice.reducer,
+    // record: recordSlice.reducer,
   },
   middleware: (getDefaultMiddleware) => getDefaultMiddleware(),
 });
--- .history/client/src/redux/store_20230427132710.js
+++ .history/client/src/redux/store_20230427132710.js
@@ -2,6 +2,7 @@ import { configureStore } from "@reduxjs/toolkit";
 import mainSlice from "../pages/Main/mainSlice";
 import dmanageSlice from "../pages/Manage/dmanage";
 import dashBoardSlice from "../pages/DashBroad/dashBroadSlice";
+
 import exhibitSlice from "../pages/exhibitManage/exhibitManageSlice";
 
 const store = configureStore({
@@ -10,7 +11,7 @@ const store = configureStore({
     dmanage: dmanageSlice.reducer,
     dashboard: dashBoardSlice.reducer,
     exhibit: exhibitSlice.reducer,
-    record: recordSlice.reducer,
+    // record: recordSlice.reducer,
   },
   middleware: (getDefaultMiddleware) => getDefaultMiddleware(),
 });
