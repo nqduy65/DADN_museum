663330911d082779e7c801aba886756f2a8d4742
nqduy65 <nguyenquocduy06051998@gmail.com>
2023-04-27 13:45:04 +0700

    cap nhat lai file index.js trong Exhibit

--- client/src/pages/exhibitManage/index.js
+++ client/src/pages/exhibitManage/index.js
@@ -1,7 +1,17 @@
 import { Box, Typography, Button } from "@mui/material";
 import { DataGrid } from "@mui/x-data-grid";
+import PropTypes from 'prop-types';
+import { GridRowModes, DataGridPro, GridToolbarContainer, GridActionsCellItem, GridAddIcon, GridSaveAltIcon } from '@mui/x-data-grid-pro';
+import { randomId } from '@mui/x-data-grid-generator';
+import EditIcon from '@mui/icons-material/Edit';
+import DeleteIcon from '@mui/icons-material/DeleteOutlined';
 import LinearProgress from "@mui/material/LinearProgress";
-import React from "react";
+// import { AddIcon, EditIcon, DeleteIcon } from '@mui/icons-material';
+import React, { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import CancelIcon from '@mui/icons-material/Close';
+import SaveIcon from '@mui/icons-material/Save';
+import { selectExhibit } from "./selectors";
 const style = {
   position: "relative",
   width: "100%",
@@ -12,7 +22,6 @@ const style = {
     display: "none",
   },
 };
-
 //Mockdata
 const columns = [
   { field: "id", headerName: "Mã hiện vật", width: 90, flex: 0.5 },
@@ -51,8 +60,7 @@ const columns = [
             color="primary"
             sx={{ minWidth: "30px" }}
             onClick={(event) => {
-              console.log(event);
-              // handleOnClick(event, cellValues);
+              handleOnClickUpdate(event, cellValues);
             }}
           >
             Sửa
@@ -62,7 +70,7 @@ const columns = [
             color="error"
             sx={{ minWidth: "30px" }}
             onClick={(event) => {
-              handleOnClick(event, cellValues);
+              handleOnClickDelete(event, cellValues);
             }}
           >
             Xóa
@@ -72,34 +80,160 @@ const columns = [
     },
   },
 ];
-
-const rows = [
-  { id: 1, exhibitName: "Tranh sơn dầu", room: "1", locate: "A" },
-  { id: 2, exhibitName: "Tranh phong thủy", room: "1", locate: "B" },
-  { id: 3, exhibitName: "Tranh đá", room: "1", locate: "C" },
-  { id: 4, exhibitName: "Tranh Monalisa", room: "1", locate: "D" },
-  { id: 5, exhibitName: "Tranh thủ", room: "1", locate: "D" },
-  { id: 6, exhibitName: "Tranh phong cảnh", room: "1", locate: "E" },
-  { id: 7, exhibitName: "Clifford", room: "1", locate: "G" },
-  { id: 8, exhibitName: "Frances", room: "1", locate: "H" },
-  { id: 9, exhibitName: "Roxie", room: "1", locate: "F" },
-  { id: 1, exhibitName: "Tranh sơn dầu", room: "1", locate: "A" },
-  { id: 2, exhibitName: "Tranh phong thủy", room: "1", locate: "B" },
-  { id: 3, exhibitName: "Tranh đá", room: "1", locate: "C" },
-  { id: 4, exhibitName: "Tranh Monalisa", room: "1", locate: "D" },
-  { id: 5, exhibitName: "Tranh thủ", room: "1", locate: "D" },
-  { id: 6, exhibitName: "Tranh phong cảnh", room: "1", locate: "E" },
-  { id: 7, exhibitName: "Clifford", room: "1", locate: "G" },
-  { id: 8, exhibitName: "Frances", room: "1", locate: "H" },
-  { id: 9, exhibitName: "Roxie", room: "1", locate: "F" },
-];
-
-const handleOnClick = () => {
-  console.log("button click");
+const handleOnClickUpdate = (e) => {
+  console.log('update', e);
 };
-const exhibitManage = () => {
+const handleOnClickDelete = (e) => {
+  console.log('delete', e);
+};
+
+function EditToolbar(props) {
+  const { setRows, setRowModesModel } = props;
+
+  const handleClick = () => {
+    const id = randomId();
+    setRows((oldRows) => [...oldRows, { id, exhibitName: '', room: '', locate: '' }]);
+    setRowModesModel((oldModel) => ({
+      ...oldModel,
+      [id]: { mode: GridRowModes.Edit, fieldToFocus: 'name' },
+    }));
+  };
+
   return (
-    <div>
+    <GridToolbarContainer>
+      <Button color="primary" startIcon={<GridAddIcon />} onClick={handleClick}>
+        Thêm mới
+      </Button>
+    </GridToolbarContainer>
+  );
+}
+EditToolbar.propTypes = {
+  setRowModesModel: PropTypes.func.isRequired,
+  setRows: PropTypes.func.isRequired,
+};
+export default function ExhibitManage() {
+  const exhibitList = useSelector(selectExhibit)
+  const [rows, setRows] = useState(exhibitList);
+  const [rowModesModel, setRowModesModel] = React.useState({});
+
+  const handleRowEditStart = (params, event) => {
+    event.defaultMuiPrevented = true;
+  };
+
+  const handleRowEditStop = (params, event) => {
+    event.defaultMuiPrevented = true;
+  };
+
+  const handleEditClick = (id) => () => {
+    setRowModesModel({ ...rowModesModel, [id]: { mode: GridRowModes.Edit } });
+  };
+
+  const handleSaveClick = (id) => () => {
+    setRowModesModel({ ...rowModesModel, [id]: { mode: GridRowModes.View } });
+  };
+
+  const handleDeleteClick = (id) => () => {
+    setRows(rows.filter((row) => row.id !== id));
+  };
+
+  const handleCancelClick = (id) => () => {
+    setRowModesModel({
+      ...rowModesModel,
+      [id]: { mode: GridRowModes.View, ignoreModifications: true },
+    });
+
+    const editedRow = rows.find((row) => row.id === id);
+    if (editedRow.isNew) {
+      setRows(rows.filter((row) => row.id !== id));
+    }
+  };
+
+  const processRowUpdate = (newRow) => {
+    const updatedRow = { ...newRow, isNew: false };
+    setRows(rows.map((row) => (row.id === newRow.id ? updatedRow : row)));
+    return updatedRow;
+  };
+
+  const handleRowModesModelChange = (newRowModesModel) => {
+    setRowModesModel(newRowModesModel);
+  };
+
+  const columns = [
+    {
+      field: 'id',
+      headerName: 'Mã hiện vật',
+      flex: 1,
+      width: 300,
+      editable: true,
+    },
+    {
+      field: 'exhibitName',
+      headerName: 'Tên hiện vật',
+      flex: 1,
+      width: 180,
+      editable: true,
+    },
+    {
+      field: 'room',
+      headerName: 'Phòng',
+      flex: 1,
+      editable: true
+    },
+    {
+      field: 'locate',
+      headerName: 'Vị trí',
+      flex: 1,
+      width: 180,
+      editable: true,
+    },
+    {
+      field: 'actions',
+      type: 'actions',
+      headerName: 'Actions',
+      flex: 1,
+      width: 100,
+      cellClassName: 'actions',
+      getActions: ({ id }) => {
+        const isInEditMode = rowModesModel[id]?.mode === GridRowModes.Edit;
+
+        if (isInEditMode) {
+          return [
+            <GridActionsCellItem
+              icon={<SaveIcon />}
+              label="Save"
+              onClick={handleSaveClick(id)}
+            />,
+            <GridActionsCellItem
+              icon={<CancelIcon />}
+              label="Cancel"
+              className="textPrimary"
+              onClick={handleCancelClick(id)}
+              color="inherit"
+            />,
+          ];
+        }
+
+        return [
+          <GridActionsCellItem
+            icon={<EditIcon />}
+            label="Edit"
+            className="textPrimary"
+            onClick={handleEditClick(id)}
+            color="inherit"
+          />,
+          <GridActionsCellItem
+            icon={<DeleteIcon />}
+            label="Delete"
+            onClick={handleDeleteClick(id)}
+            color="inherit"
+          />,
+        ];
+      },
+    },
+  ];
+
+  return (
+    <>
       <Typography
         variant="h2"
         sx={{
@@ -122,12 +256,37 @@ const exhibitManage = () => {
       >
         Quản lý hiện vật
       </Typography>
-      <Box sx={style}>
-        <DataGrid
+      <Box
+        sx={{
+          height: 500,
+          width: '100%',
+          '& .actions': {
+            color: 'text.secondary',
+          },
+          '& .textPrimary': {
+            color: 'text.primary',
+          },
+          paddingInline: "10px",
+          "&.MuiDataGrid-virtualScroller ::-webkit-scrollbar": {
+            display: "none",
+          },
+        }}
+      >
+        <DataGridPro
           rows={rows}
           columns={columns}
-          hideScrollbar={true}
-          loadIcon={<LinearProgress sx={{ backgroundColor: "#FF0000" }} />}
+          editMode="row"
+          rowModesModel={rowModesModel}
+          onRowModesModelChange={handleRowModesModelChange}
+          onRowEditStart={handleRowEditStart}
+          onRowEditStop={handleRowEditStop}
+          processRowUpdate={processRowUpdate}
+          slots={{
+            toolbar: EditToolbar,
+          }}
+          slotProps={{
+            toolbar: { setRows, setRowModesModel },
+          }}
           sx={{
             fontSize: "15px",
             boxShadow:
@@ -140,8 +299,61 @@ const exhibitManage = () => {
           }}
         />
       </Box>
-    </div>
+    </>
   );
-};
+}
 
-export default exhibitManage;
+// const ExhibitManage = () => {
+//   // const dispatch = useDispatch();
+//   const exhibitList = useSelector(selectExhibit)
+
+
+
+
+//   return (
+//     <div>
+//       <Typography
+//         variant="h2"
+//         sx={{
+//           width: "100%",
+//           height: "85px",
+
+//           /* Nunito/Heading/24 - Bold */
+
+//           fontStyle: " normal;",
+//           fontWeight: 700,
+//           fontSize: "28px;",
+//           lineHeight: "140%;",
+//           /* identical to box height, or 34px */
+
+//           color: "#4B36CC",
+//           padding: "24px",
+
+//           /* Inside auto layout */
+//         }}
+//       >
+//         Quản lý hiện vật
+//       </Typography>
+//       <Box sx={style}>
+//         <DataGrid
+//           rows={exhibitList}
+//           columns={columns}
+//           hideScrollbar={true}
+//           loadIcon={<LinearProgress sx={{ backgroundColor: "#FF0000" }} />}
+//           sx={{
+//             fontSize: "15px",
+//             boxShadow:
+//               "0px 1px 3px rgba(0, 0, 0, 0.12), 0px 1px 1px rgba(0, 0, 0, 0.14), 0px 2px 1px -1px rgba(0, 0, 0, 0.2);",
+//             borderRadius: "4px",
+//             "&.virtualScrollerContent": {
+//               color: "white",
+//             },
+//             backgroundColor: "white",
+//           }}
+//         />
+//       </Box>
+//     </div>
+//   );
+// };
+
+// export default ExhibitManage;
